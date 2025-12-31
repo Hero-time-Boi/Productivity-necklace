dd49cabbb3b2f67c70cc9665e5cd36535723de8f
Preston English <tufftoast6@gmail.com>
2025-12-30 21:10:25 -0700

    the good ol photoresistor set

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,24 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED_PIN, HIGH);
-  delay(500);
-  digitalWrite(LED_PIN, LOW);
-  delay(500);
+  int value = analogRead(A0);
+
+  Serial.println("analog value: ");
+  Serial.println(value);
+
+  if (value > 100){
+    digitalWrite(LED_PIN, LOW);
+  }
+
+  else {
+    digitalWrite(LED_PIN, HIGH);
+  }
+  
+  delay(250);
 }
